2362f099f5b5afa19090eb2ca041236c7f4ba7b4
danielle-smith <danielle@natesdesign.com>
2021-01-17 10:51:00 -0700

    update

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import axios from 'axios';
 import { withRouter } from "react-router";
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavLink } from "react-router-dom";import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const NavigationComponent = (props) => {
-    const dynamicLink = (route, linkText) => {
-        return (
-        <div className="nav-link-wrapper" activeclassname="nav-link-active">
-            <NavLink to={route}>{linkText}</NavLink>
-        </div>
-        );
-    }
-
-    const handleSignOut = () => {
-        axios
-            .delete("https://api.devcamp.space/logout", { withCredentials: true })
-            .then(response => {
-                if (response.status === 200) {
-                    props.history.push("/");
-                    props.handleSuccessfulLogout();
-                }
-                return response.data;
-            })
-            .catch(error => {
-                console.log("Error signing out", error);
-            });
-    };
+   
 
     return (
         <div className="nav-wrapper">
