eea6688da31181e55d0c4d0526395b2c3cca434a
danielle-smith <danielle@natesdesign.com>
2021-05-25 13:31:23 -0600

    media queries

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -3,35 +3,32 @@ import { Link } from 'react-router-dom';
 import striptags from 'striptags';
 import Truncate from 'react-truncate';
 
-
-
 class BlogItem extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        
+
         const { id, content, title } = this.props;
 
-     
         return (
             <div className="blog-item-list">
                 <Link to={`/b/${id}`}>
                     <h1>{title}</h1>
                 </Link>
-                
-                <Truncate style={{fontSize:"1.7em"}} lines={5} ellipsis={
+
+                <Truncate style={{ fontSize: "1.7em" }} lines={5} ellipsis={
                     <span >
                         ...<Link to={`/b/${id}`}>Read more</Link>
                     </span>
                 }>
                     {striptags(content)}
                 </Truncate>
-    
+
             </div>
         );
-        
+
     }
 
 };
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
-
-
 const NavigationComponent = (props) => {
-   
 
     return (
         <div className="nav-wrapper">
             <div className="left-side">
                 <div className="nav-link-wrapper">
-                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink> 
+                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
                 </div>
 
                 <div className="nav-link-wrapper">
@@ -23,7 +20,7 @@ const NavigationComponent = (props) => {
                 </div>
 
             </div>
-            
+
             <div className="right-side-wrapper">
                 <div className="right-side">
                     <a href="https://dds-blogdb.herokuapp.com/signup" target="_blank" className="admin-link">Register</a>
